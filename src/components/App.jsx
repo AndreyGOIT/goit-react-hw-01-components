9d47d2b8932360cf6ef01812f9c1f653c4168ad1
AndreyGOIT <ae.borga@gmail.com>
2022-11-19 21:51:14 +0200

    upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
-import { Profile } from "./Profile/Profile";
-import user from '../user.json'
-
-
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
 
 export const App = () => {
   return (
@@ -12,10 +10,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      <Profile user={user } />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
