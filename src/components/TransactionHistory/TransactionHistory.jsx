253864d861c413889317deda2d328232513d1975
AndreyGOIT <ae.borga@gmail.com>
2022-11-30 10:20:55 +0200

    to check

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -6,16 +6,16 @@ export const TransactionHistory = ({ items }) => {
   return (
     <table className={styles.transactionHistory}>
       <thead>
-        <tr key="0" className={styles.upperRow}>
+        <tr className={styles.upperRow}>
           <th>Type</th>
           <th>Amount</th>
           <th>Currency</th>
         </tr>
       </thead>
 
-      {items.map(({ amount, currency, id, type }, index) => (
+      {items.map(({ amount, currency, id, type }) => (
         <tbody className={styles.tablerow}>
-          <tr key={index + 1} className={styles.bodyRow}>
+          <tr key={id} className={styles.bodyRow}>
             <td>{type}</td>
             <td>{amount}</td>
             <td>{currency}</td>
